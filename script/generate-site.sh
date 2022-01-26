70b32555aafcd8c7b5b46827da5ff614f98f538b
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-01-26 13:48:21 +0100

    _properties: Payment choice, Tor, IPv6, blocked countries, fill in DC

--- script/generate-site.sh
+++ script/generate-site.sh
@@ -142,13 +142,13 @@ print_items() {
     CLASS=""
     FVEY="Australia|Canada|New Zealand|UK|USA"
     NINEEYES="Denmark|France|Netherlands|Norway"
-    if echo "$PROP" | grep -iqE "\<(depends|limited|probably|VC|not|$NINEEYES)\>|\<(venture capital|partial|leak|possibl)|(^|[^0-9a-z_-])only "; then
+    if echo "$PROP" | grep -iqE "\<(depends|usually|limited|probably|VC|often|not|$NINEEYES)\>|\<(venture capital|partial|leak|possibl)|(^|[^0-9a-z_-])only "; then
       CLASS="p"
     fi
     if echo "$PROP" | grep -iqE "\<(no|none|proprietary|unknown|offshore|cryptocoin|$FVEY)\>"; then
       CLASS="n"
     fi
-    if echo "$PROP" | grep -iqE "\<yes\>"; then
+    if echo "$PROP" | grep -iqE "\<yes|N/A\>"; then
       CLASS="y"
     fi
 
